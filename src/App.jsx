428ddb2919313c9c9406afaa310034637d0eacda
Tahrat Anjum <tahratanjum@gmail.com>
2025-10-07 01:18:26 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ const LandingPage = ({ showWelcome, setShowWelcome }) => {
               <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
                 © 2025{" "}
                 <a href="https://flowbite.com/" className="hover:underline">
-                  ABC™
+                  VIPER™
                 </a>
                 . All Rights Reserved.
               </span>
@@ -55,7 +55,7 @@ const ProjectPageLayout = () => (
         <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
           © 2023{" "}
           <a href="https://flowbite.com/" className="hover:underline">
-            EkiZR™
+            EkiZRIT
           </a>
           . All Rights Reserved.
         </span>
