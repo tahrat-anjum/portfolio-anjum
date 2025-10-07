3cf08a2b9fd07b993cb72332576bcaed07960a1d
Tahrat Anjum <tahratanjum@gmail.com>
2025-10-08 02:14:10 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React, { useState, lazy, Suspense } from 'react';
+import React, { lazy, Suspense } from 'react';
 import "./index.css";
 
 import AnimatedBackground from "./components/Background";
@@ -12,36 +12,32 @@ const About = lazy(() => import("./Pages/About"));
 const Portofolio = lazy(() => import("./Pages/Portofolio"));
 const ContactPage = lazy(() => import("./Pages/Contact"));
 const ProjectDetails = lazy(() => import("./components/ProjectDetail"));
-const WelcomeScreen = lazy(() => import("./Pages/WelcomeScreen"));
+// ❌ Removed WelcomeScreen
 
-const LandingPage = ({ showWelcome, setShowWelcome }) => {
+const LandingPage = () => {
   return (
     <AnimatePresence mode="wait">
       <Suspense fallback={<div className="text-white text-center mt-10">Loading...</div>}>
-        {showWelcome ? (
-          <WelcomeScreen onLoadingComplete={() => setShowWelcome(false)} />
-        ) : (
-          <>
-            <Navbar />
-            <AnimatedBackground />
-            <Home />
-            <About />
-            <Portofolio />
-            <ContactPage />
-            <footer>
-              <center>
-                <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
-                <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
-                  © 2025{" "}
-                  <a href="https://flowbite.com/" className="hover:underline">
-                    VIPER IT
-                  </a>
-                  . All Rights Reserved.
-                </span>
-              </center>
-            </footer>
-          </>
-        )}
+        <>
+          <Navbar />
+          <AnimatedBackground />
+          <Home />
+          <About />
+          <Portofolio />
+          <ContactPage />
+          <footer>
+            <center>
+              <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
+              <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
+                © 2025{" "}
+                <a href="https://flowbite.com/" className="hover:underline">
+                  VIPER IT
+                </a>
+                . All Rights Reserved.
+              </span>
+            </center>
+          </footer>
+        </>
       </Suspense>
     </AnimatePresence>
   );
@@ -66,12 +62,10 @@ const ProjectPageLayout = () => (
 );
 
 function App() {
-  const [showWelcome, setShowWelcome] = useState(true);
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage showWelcome={showWelcome} setShowWelcome={setShowWelcome} />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/project/:id" element={<ProjectPageLayout />} />
       </Routes>
     </BrowserRouter>
