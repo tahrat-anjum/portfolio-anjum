b63743d77afded1dee8f4f86bf2555d29dd880a6
Tahrat Anjum <tahratanjum@gmail.com>
2025-10-07 13:20:27 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) {
+              return 'react-vendor'
+            }
+            if (id.includes('lucide-react')) {
+              return 'icons-vendor'
+            }
+            if (id.includes('lottiefiles')) {
+              return 'lottie-vendor'
+            }
+            // Add more libs if needed
+            return 'vendor'
+          }
+        }
+      }
+    }
+  }
 })
