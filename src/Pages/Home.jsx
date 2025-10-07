05c80ed7888d5d123465c8cbc32f2afe3ef6b2c9
Tahrat Anjum <tahratanjum@gmail.com>
2025-10-07 18:01:45 +0600

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -94,12 +94,12 @@ const SocialLink = memo(({ icon: Icon, link }) => (
 const TYPING_SPEED = 100;
 const ERASING_SPEED = 50;
 const PAUSE_DURATION = 2000;
-const WORDS = ["Network & Telecom Student", "Tech Enthusiast"];
+const WORDS = ["Digital Security Enthusiast", "Brand Protection Consultant", "Account Recovery Expertt"];
 const TECH_STACK = ["React", "Javascript", "Node.js", "Tailwind"];
 const SOCIAL_LINKS = [
-  { icon: Github, link: "https://github.com/ankitpathak62" },
-  { icon: Linkedin, link: "https://www.linkedin.com/in/ankitpathak62/" },
-  { icon: Instagram, link: "https://www.instagram.com/sunfiresensei/" },
+  { icon: Github, link: "https://github.com/tahrat-anjum" },
+  { icon: Linkedin, link: "https://www.linkedin.com/in/tahrat-anjum/" },
+  { icon: Instagram, link: "https://www.instagram.com/Tahrat_Anjum" },
 ];
 
 const Home = () => {
