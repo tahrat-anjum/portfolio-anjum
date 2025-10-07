711ad3af068c4ec36d75383505c8b45f2462fc7d
Tahrat Anjum <tahratanjum@gmail.com>
2025-10-07 12:42:35 +0600

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -95,7 +95,6 @@ const TYPING_SPEED = 100;
 const ERASING_SPEED = 50;
 const PAUSE_DURATION = 2000;
 const WORDS = ["Network & Telecom Student", "Tech Enthusiast"];
-const TECH_STACK = ["React", "Javascript", "Node.js", "Tailwind"];
 const SOCIAL_LINKS = [
   { icon: Github, link: "https://github.com/ankitpathak62" },
   { icon: Linkedin, link: "https://www.linkedin.com/in/ankitpathak62/" },
