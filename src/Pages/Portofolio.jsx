0e1c11c54f911a4fdfca61c3414c0c88f66ef14b
Tahrat Anjum <tahratanjum@gmail.com>
2025-10-09 03:00:45 +0600

    Update Portofolio.jsx

--- src/Pages/Portofolio.jsx
+++ src/Pages/Portofolio.jsx
@@ -125,10 +125,10 @@ export default function FullWidthTabs() {
       {/* Header */}
       <div className="text-center pb-10" data-aos="fade-up" data-aos-duration="1000">
         <h2 className="inline-block text-3xl md:text-5xl font-bold text-center mx-auto text-transparent bg-clip-text bg-gradient-to-r from-[#6366f1] to-[#a855f7]">
-          Portfolio Showcase
+          Service Showcase
         </h2>
         <p className="text-slate-400 max-w-2xl mx-auto text-sm md:text-base mt-2">
-          Explore my journey through services, certifications, and technical expertise. 
+          Explore my services, Success Story, and technical expertise. 
           Each section represents a milestone in my continuous learning path.
         </p>
       </div>
