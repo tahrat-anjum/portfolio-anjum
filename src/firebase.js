ed5288ac4879d2b7cad805ce17582436098258ed
Tahrat Anjum <tahratanjum@gmail.com>
2025-10-09 00:22:51 +0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"
-import { collection, addDoc, getDocs } from "@firebase/firestore"; // Perbarui ini
-
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB-lfUt1adpQ0KYcFFW_oAWTJVfHDOOZy8",
-  authDomain: "portofolio-web-3e8e8.firebaseapp.com",
-  projectId: "portofolio-web-3e8e8",
-  storageBucket: "portofolio-web-3e8e8.appspot.com",
-  messagingSenderId: "25195509306",
-  appId: "1:25195509306:web:2b635dcf997137bf612703"
+  apiKey: "AIzaSyCYxTqQwv5DSqF2nfvQHHnKkndwSB0jSdE",
+  authDomain: "portfolio-d5679.firebaseapp.com",
+  projectId: "portfolio-d5679",
+  storageBucket: "portfolio-d5679.firebasestorage.app",
+  messagingSenderId: "226774061827",
+  appId: "1:226774061827:web:c63d5ef68ce72c5c60cf36",
+  measurementId: "G-GTHBRSV9R3"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db, collection, addDoc };
+const analytics = getAnalytics(app);
