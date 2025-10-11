2a55e4199a243e1660f31e25419246a818267498
Tahrat Anjum <tahratanjum@gmail.com>
2025-10-11 18:20:16 +0600

    Update firebase-comment.js

--- src/firebase-comment.js
+++ src/firebase-comment.js
@@ -4,12 +4,13 @@ import { getStorage } from "firebase/storage";
 import { collection, addDoc } from "@firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDJPq9a0YPoQYkpQ-Uaw7aXQRXzzqOKzFA",
-    authDomain: "web-kelas-tes.firebaseapp.com",
-    projectId: "web-kelas-tes",
-    storageBucket: "web-kelas-tes.appspot.com",
-    messagingSenderId: "890817433268",
-    appId: "1:890817433268:web:11e5258f8864a6174c11e1"
+  apiKey: "AIzaSyAjYBrypVi_EIjreVqJLvectfhr3qh8bL4",
+  authDomain: "tahratanjum-portfolio.firebaseapp.com",
+  projectId: "tahratanjum-portfolio",
+  storageBucket: "tahratanjum-portfolio.firebasestorage.app",
+  messagingSenderId: "97094510005",
+  appId: "1:97094510005:web:e3861249c5191b4471e5fc",
+  measurementId: "G-D0YYTC8564"
 };
 
 // Initialize with a unique name
