352d853d6f8f3014a4d25ce389e159bad581c996
Tahrat Anjum <tahratanjum@gmail.com>
2025-10-11 17:21:00 +0600

    Update firebase-comment.js

--- src/firebase-comment.js
+++ src/firebase-comment.js
@@ -1,20 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-import { collection, addDoc } from "@firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDJPq9a0YPoQYkpQ-Uaw7aXQRXzzqOKzFA",
-    authDomain: "web-kelas-tes.firebaseapp.com",
-    projectId: "web-kelas-tes",
-    storageBucket: "web-kelas-tes.appspot.com",
-    messagingSenderId: "",
-    appId: "1:890817433268:web:11e5258f8864a6174c11e1"
+  apiKey: "AIzaSyAjYBrypVi_EIjreVqJLvectfhr3qh8bL4",
+  authDomain: "tahratanjum-portfolio.firebaseapp.com",
+  projectId: "tahratanjum-portfolio",
+  storageBucket: "tahratanjum-portfolio.firebasestorage.app",
+  messagingSenderId: "97094510005",
+  appId: "1:97094510005:web:e3861249c5191b4471e5fc",
+  measurementId: "G-D0YYTC8564"
 };
 
-// Initialize with a unique name
-const app = initializeApp(firebaseConfig, 'comments-app');
-const db = getFirestore(app);
-const storage = getStorage(app);
-
-export { db, storage, collection, addDoc };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
